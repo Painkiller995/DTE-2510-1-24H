dac1c7cb6ab358b33bbebfec8380182023b33dd7
PainKiller995 <fahdakramdaher@gmail.com>
2024-09-23 00:48:20 +0200

    add bioinformatics implementation

--- o2_bioinformatics_find_genes.py
+++ o2_bioinformatics_find_genes.py
@@ -6,6 +6,7 @@ https://github.com/Painkiller995/DTE-2510-1-24H
 
 """
 
+GENE_CHARACTERS = "ACGT"
 START_TRIPLET = "ATG"
 END_TRIPLETS = ["TAA", "TAG", "TGA"]
 
@@ -20,40 +21,71 @@ def find_genes(dna_sequence: str) -> str:
     Returns:
         The genes found in the DNA sequence.
     """
-    genes = []
-
-    start_triplet_indexes = []
-    stop_triplet_indexes = []
+    gene_list = []
 
     index = 0
-    while index < len(dna_sequence):
-        start_triplet_index = dna_sequence.find(START_TRIPLET, index)
+    length = len(dna_sequence)
 
-        if start_triplet_index > 0 and start_triplet_index not in start_triplet_indexes:
-            start_triplet_indexes.append(start_triplet_index)
+    while index < length:
+        start_index = dna_sequence.find(START_TRIPLET, index)
+        if start_index == -1:
+            break
 
+        stop_index = None
         for end_triplet in END_TRIPLETS:
-            end_triplet_index = dna_sequence.find(end_triplet, index)
-            if end_triplet_index > 0 and end_triplet_index not in stop_triplet_indexes:
-                stop_triplet_indexes.append(end_triplet_index)
-        index += 1
+            end_index = dna_sequence.find(end_triplet, start_index + 3)
+            if end_index != -1 and (stop_index is None or end_index < stop_index):
+                stop_index = end_index
 
-    for start_triplet_index in start_triplet_indexes:
-        for stop_triplet_index in stop_triplet_indexes:
-            if start_triplet_index < stop_triplet_index:
-                sub_gene = dna_sequence[start_triplet_index : stop_triplet_index + 3]
-                gene = sub_gene[3 : len(sub_gene) - 3]
-                if (
-                    len(gene) % 3 == 0
-                    and START_TRIPLET not in gene
-                    and all(end_triplet not in gene for end_triplet in END_TRIPLETS)
-                    and gene != ""
-                ):
-                    genes.append(gene)
+        if stop_index:
+            gene = dna_sequence[start_index + 3 : stop_index]
+            if is_valid_gene(gene):
+                gene_list.append(gene)
 
-    return ",".join(genes)
+        index = start_index + 1
+
+    return ",".join(gene_list)
 
 
-example = "TTATGATGTTTTAAGGATGGGGCGTTAGTT"
-genes = find_genes(example)
-print(genes)
+def is_valid_gene(gene: str) -> bool:
+    """
+    Check if the extracted gene is valid.
+
+    Args:
+        gene: The gene.
+
+    Returns:
+        True if the gene is valid, otherwise False.
+    """
+    return (
+        len(gene) % 3 == 0
+        and START_TRIPLET not in gene
+        and all(end not in gene for end in END_TRIPLETS)
+        and gene != ""
+    )
+
+
+def get_user_input() -> str:
+    """
+    Get the user input and validate it.
+
+    Returns:
+        The user input as a string.
+    """
+    while True:
+        user_input: str = input("Please enter a gene sequence: \n")
+
+        if not all(char in GENE_CHARACTERS for char in user_input):
+            print(
+                "Oops! 😅 It seems you entered an invalid input, please enter a valid gene sequence. "
+            )
+            continue
+
+        print("-------------------------------------------------")
+        return user_input
+
+
+if __name__ == "__main__":
+    sequence = get_user_input()
+    genes = find_genes(sequence)
+    print(f"The genes found in the DNA sequence are: \n {genes}")
