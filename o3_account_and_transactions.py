6a0a2fc47364b780be9ef26ef00593c97f3db8e2
PainKiller995 <fahdakramdaher@gmail.com>
2024-10-14 21:32:13 +0200

    add transaction class

--- o3_account_and_transactions.py
+++ o3_account_and_transactions.py
@@ -6,6 +6,8 @@ https://github.com/Painkiller995/DTE-2510-1-24H
 
 """
 
+from datetime import datetime
+
 
 class Account:
     """
@@ -35,6 +37,8 @@ class Account:
         self._balance = balance
         self._interest = interest
 
+        self._transactions: list[Transaction] = []
+
     @property
     def cust_id(self) -> int:
         """Read-only property for customer ID."""
@@ -61,6 +65,7 @@ class Account:
         Args:
             amount: The amount to deposit.
         """
+        self._transactions.append(Transaction(amount))
         self._balance += amount
 
     def withdraw(self, amount) -> bool:
@@ -72,9 +77,15 @@ class Account:
             True if the withdrawal was successful, False otherwise.
         """
         if amount > 0 and self._balance - amount >= 0:
+            self._transactions.append(Transaction(-amount))
             self._balance -= amount
             return True
         return False
+
+    def show_transactions(self) -> None:
+        """This method shows all transactions."""
+        for transaction in self._transactions:
+            print(transaction)
 
     def __str__(self) -> str:
         return (
@@ -85,10 +96,45 @@ class Account:
         )
 
 
+class Transaction:
+    """
+    This class represents a transaction.
+
+    Attributes:
+        time: The transaction time.
+        amount: The transaction amount.
+    """
+
+    def __init__(self, amount: float = 0.0):
+        self._time: str = self._get_time_as_str()
+        self._amount = amount
+
+    @property
+    def time(self) -> str:
+        """Read-only property for transaction time."""
+        return self._time
+
+    @property
+    def amount(self) -> float:
+        """Read-only property for transaction amount."""
+        return self._amount
+
+    def _get_time_as_str(self) -> str:
+        """
+        This method returns the transaction time as a string.
+        Returns:
+            The transaction time as a string.
+        """
+        return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    def __str__(self) -> str:
+        return f"Time: {self._time}, Amount: {self._amount:.2f}"
+
+
 if __name__ == "__main__":
     # Create an account
     account = Account(1, 1000, 50000.0, 7)
-    print("Account created: \n", account)
+    print(f"Account created:\n{account}")
 
     # Deposit money
     account.deposit(500.0)
@@ -99,3 +145,6 @@ if __name__ == "__main__":
         print(f"New balance after withdrawal of 1000 is {account.balance}")
     else:
         print("Withdrawal failed.")
+
+    print("Transactions:")
+    account.show_transactions()
