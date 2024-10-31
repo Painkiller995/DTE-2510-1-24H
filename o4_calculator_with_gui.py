cd565449828e15de4be13ddf5e848aba861da1d6
PainKiller995 <fahdakramdaher@gmail.com>
2024-10-31 02:28:22 +0100

    make calculator gui class

--- o4_calculator_with_gui.py
+++ o4_calculator_with_gui.py
@@ -1,104 +1,37 @@
 """
-This module represents a calculator implementation.
+This module represents a gui calculator implementation.
+
+Dependencies:
+    - tkinter
+    o4_calculator.py available in the same repository on github.
 
 This implementation could be improved in the feature please check github for the latest version.
 https://github.com/Painkiller995/DTE-2510-1-24H
 
 """
 
-from typing import Literal
+import tkinter as tk
+from o4_calculator import Calculator
 
 
-class Calculator:
+class CalculatorGUI:
     """
-    Represents a simple calculator that can perform basic arithmetic operations.
+    Represents a simple calculator GUI.
     """
 
-    def __init__(self):
-        self._log = []
+    def __init__(self, root: tk.Tk):
+        self.root = root
+        self.root.title("Calculator GUI")
 
-    def calculate(
-        self,
-        operand1: float,
-        operand2: float,
-        operator: Literal["*", "/", "+", "-"],
-    ) -> float | None:
-        """
-        Calculate the result of the operation.
+        width, height = 300, 400
+        x = (self.root.winfo_screenwidth() - width) // 2
+        y = (self.root.winfo_screenheight() - height) // 2
+        self.root.geometry(f"{width}x{height}+{x}+{y}")
 
-        Args:
-            operand1: The first operand.
-            operand2: The second operand.
-            operator: The operator to use for the operation.
-
-        Returns:
-            The result of the operation.
-        """
-        if operator not in ["*", "/", "+", "-"]:
-            raise ValueError("Invalid operator provided")
-
-        if operand1 is None or operand2 is None:
-            raise ValueError("Operands cannot be None")
-
-        if operator == "/" and operand2 == 0:
-            raise ValueError("Division by zero")
-
-        result = None
-
-        if operator == "*":
-            result = operand1 * operand2
-        elif operator == "/":
-            result = operand1 / operand2
-        elif operator == "+":
-            result = operand1 + operand2
-        elif operator == "-":
-            result = operand1 - operand2
-
-        expression = f"{operand1} {operator} {operand2} = {result}"
-        self.add_to_log(expression)
-
-        return result
-
-    def get_log(self) -> list[str]:
-        """
-        Get the log of calculations performed.
-
-        Returns:
-            The log of calculations performed.
-        """
-        return self._log
-
-    def get_last_logged(self) -> str | None:
-        """
-        Get the last calculation performed.
-
-        Returns:
-            The last calculation performed.
-        """
-        if len(self._log) == 0:
-            return "No calculations available"
-        return self._log[-1]
-
-    def add_to_log(self, expression: str) -> None:
-        """
-        Add a calculation to the log.
-
-        Args:
-            expression: The calculation to add to the log.
-        """
-        self._log.append(expression)
-
-    def clear_log(self) -> None:
-        """
-        Clear the log of calculations.
-        """
-        self._log.clear()
+        self.calculator = Calculator()
 
 
 if __name__ == "__main__":
-    calculator = Calculator()
-    calculator.calculate(1, 2, "+")
-    calculator.calculate(2, 2, "*")
-    calculator.calculate(16, 2, "/")
-    print(calculator.get_log())
-    print(calculator.get_last_logged())
+    root = tk.Tk()
+    app = CalculatorGUI(root)
+    root.mainloop()
